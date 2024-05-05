1ac05f2a2c2f383d4be36292f833afd1a547f32a
huniasashraf <hunaisashraf@gmail.com>
2024-05-05 13:51:21 +0530

    comments import fixed

--- comments/index.js
+++ comments/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = require("./routes/route");
 const dotenv = require("dotenv");
 const connect = require("./config/dbConfig");
-const consume = require("../posts/kafka/consumer");
+const consume = require("./kafka/consumer");
 
 const app = express();
 dotenv.config();
@@ -11,14 +11,14 @@ app.use(express.json());
 
 app.use(router);
 
-// (async function () {
-//   try {
-//     await consume();
-//   } catch (error) {
-//     console.log(error);
-//     process.exit();
-//   }
-// })();
+(async function () {
+  try {
+    await consume();
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+})();
 
 const port = process.env.PORT || 3003;
 
